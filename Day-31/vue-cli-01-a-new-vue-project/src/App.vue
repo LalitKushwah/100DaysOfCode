4afa75a42bc4faecf71acf2e23d2bdb3c34eaa0a
LalitKushwah <lalitkushwah52525@gmail.com>
2020-10-10 14:44:19 +0530

    Day-30 with component structure and vue-cli

---
+++ Day-31/vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+    <section>
+        <h2>Welcome Here...</h2>
+        <h3>My Friends list</h3>
+        <ul>
+            <friend-contact></friend-contact>
+            <friend-contact></friend-contact>
+        </ul>
+    </section>
+</template>
+<script>
+export default {
+    data () {
+        return {
+            friends: [
+                {
+                    id: '1',
+                    name: 'Lalit Kushwah',
+                    email: 'lalitkushwah@gmail.com'
+                },
+                {
+                    id: '2',
+                    name: 'Megha Mehrotra',
+                    email: 'megha@gmail.com'
+                }
+            ]
+        }
+    }
+}
+</script>
+<style>
+    section {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+    ul {
+        list-style: none;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+    }
+</style>
---
+++ Day-31/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,40 @@
+<template>
+  <li>
+    <button @click="toggleDetails">{{isvisible ? 'Hide' : 'Show'}} Details</button>
+    <ul v-if="isvisible">
+      <li>Name: {{friend.name}}</li>
+      <li>Email: {{friend.email}}</li>
+    </ul>
+  </li>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      isvisible: true,
+      friend: {
+        id: "1",
+        name: "Lalit Kushwah",
+        email: "lalitkushwah@gmail.com",
+      },
+    };
+  },
+  methods: {
+      toggleDetails () {
+          this.isvisible = !this.isvisible;
+      }
+  }
+};
+</script>
+<style scoped>
+    ul {
+        list-style: none;
+        display:flex;
+        flex-direction: column;
+        align-items: flex-start;
+    }
+    li {
+        position: relative;
+        padding: 10px;
+    }
+</style>
