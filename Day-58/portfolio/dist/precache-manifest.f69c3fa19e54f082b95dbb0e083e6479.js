2953b3a3ebe65450c3d20df7f9398d6c7bf5f0fd
LalitKushwah <lalitkushwah52525@gmail.com>
2020-11-06 21:03:43 +0530

    Day-58 with portfolio changes

---
+++ Day-58/portfolio/dist/precache-manifest.f69c3fa19e54f082b95dbb0e083e6479.js
@@ -0,0 +1,186 @@
+self.__precacheManifest = [
+  {
+    "revision": "08cd60f775e15e5fe2e7",
+    "url": "/css/app.27a77b18.css"
+  },
+  {
+    "revision": "08cd60f775e15e5fe2e7",
+    "url": "/js/app.b2a8b2e4.js"
+  },
+  {
+    "revision": "0c1e4c01d91951e98d71",
+    "url": "/js/chunk-vendors.021bf7ce.js"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/fonts/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/fonts/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/fonts/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/fonts/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/img/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "9a0c7cf9a45b0bf21da7cbd50ddebe68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9949387c6179e2dc4c675134a7b7935",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "39b479547e2d5b3a2307a9aaeb00771a",
+    "url": "/images/angular.png"
+  },
+  {
+    "revision": "e5283d4a4ed55c81dfe382ce36da5798",
+    "url": "/fonts/fonts-css.css"
+  },
+  {
+    "revision": "7917732bc70f07742f703e34cfa18123",
+    "url": "/images/ionic.png"
+  },
+  {
+    "revision": "0e421b944aac62fe631057b8cb81c9ec",
+    "url": "/images/jenkins.jpeg"
+  },
+  {
+    "revision": "12d548b7d7871ec4cadb755cf9b52678",
+    "url": "/images/hotwax.png"
+  },
+  {
+    "revision": "01d3c3d757d5ff4eba375c1303402364",
+    "url": "/images/mongodb.jpeg"
+  },
+  {
+    "revision": "f7bc9fae4b5f664d512e205560484f5a",
+    "url": "/images/mastercard.png"
+  },
+  {
+    "revision": "5062b5479e5aa9afffd1ff5d0b0682e2",
+    "url": "/images/nodejs.png"
+  },
+  {
+    "revision": "b26e1816aff84d0337ec2d2fce23901c",
+    "url": "/images/Lalit_Kushwah_Resume.pdf"
+  },
+  {
+    "revision": "0e172f78580a279c056b95ffc624fd98",
+    "url": "/images/stenciljs.png"
+  },
+  {
+    "revision": "0b28f2dfed69a3be43422620792a0a1d",
+    "url": "/images/spring.png"
+  },
+  {
+    "revision": "185288d13ed8e9d745bd279ea34667bf",
+    "url": "/img/brand/blue.png"
+  },
+  {
+    "revision": "71faa42ffd3b76d0a06d4398f4b219fa",
+    "url": "/images/zensar.png"
+  },
+  {
+    "revision": "8e55eab46b5fcfc4a7a0b27cb07c8888",
+    "url": "/img/brand/github-white-slim.png"
+  },
+  {
+    "revision": "3bfec843ef6dc688c08004618a4bc38c",
+    "url": "/images/vuejs.png"
+  },
+  {
+    "revision": "8e7aa8fac5f60b44b4512742024baeac",
+    "url": "/images/profile.jpg"
+  },
+  {
+    "revision": "c85c75275c0a0a617f9e5accc2700908",
+    "url": "/img/brand/creativetim-white-slim.png"
+  },
+  {
+    "revision": "6fafe4baca9d50d61a898c84ade7afa3",
+    "url": "/img/brand/white.png"
+  },
+  {
+    "revision": "07c01d6d12b57d0cd5c9715cbff261b3",
+    "url": "/img/brand/logo.png"
+  },
+  {
+    "revision": "b9949387c6179e2dc4c675134a7b7935",
+    "url": "/img/brand/favicon.png"
+  },
+  {
+    "revision": "dc49ad52655e1d9d0552c026db3ef688",
+    "url": "/img/theme/landing.jpg"
+  },
+  {
+    "revision": "974088a1931e40895bac6db119c62448",
+    "url": "/img/theme/promo-1.png"
+  },
+  {
+    "revision": "20d702b83a06bdb2ea71c4c0cb9a7a56",
+    "url": "/img/theme/profile.jpg"
+  },
+  {
+    "revision": "594b1ee1d95ada356eaad078e9217932",
+    "url": "/img/ill/ill-2.svg"
+  },
+  {
+    "revision": "fd4a34d026fb9e0f4867188d47b11ba8",
+    "url": "/img/theme/img-1-1200x1000.jpg"
+  },
+  {
+    "revision": "be997d5226b992ffad34816870c6b7aa",
+    "url": "/img/theme/team-2-800x800.jpg"
+  },
+  {
+    "revision": "7789b5bfa57722dd8916b1b9ff1b1d37",
+    "url": "/img/theme/img-2-1200x1000.jpg"
+  },
+  {
+    "revision": "edc7106b21ec12e57022b2ebd534cd2d",
+    "url": "/img/theme/team-1-800x800.jpg"
+  },
+  {
+    "revision": "54e3f3c414bd8e7234bae3ee3be950e5",
+    "url": "/img/theme/team-3-800x800.jpg"
+  },
+  {
+    "revision": "66618a418175ddf2ac8c47a241d327a8",
+    "url": "/img/theme/team-4-800x800.jpg"
+  },
+  {
+    "revision": "3b5a73dd5a87a248baeb7403fca408d2",
+    "url": "/js/kit-js.js"
+  }
+];
