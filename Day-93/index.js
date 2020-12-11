dc64b07b9d7ecd825a12d2e611c2e74725b319d5
LalitKushwah <lalitkushwah52525@gmail.com>
2020-12-11 21:43:05 +0530

    array rotation problem

---
+++ Day-93/index.js
@@ -0,0 +1,12 @@
+function rotateRight(arr,rotations){
+    if(rotations == 0) return arr;
+    for(let i = 0; i < rotations;i++){
+      let element = arr.pop();
+      arr.unshift(element);
+    }
+  
+    return arr;
+  }
+  
+  
+  rotateRight([2, 3, 4, 5, 7], 3); // Return [4,5,7,2,3]
