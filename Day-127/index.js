0102af544b7bdcdbaabe8cd0dc6448cfbd55dc73
LalitKushwah <lalitkushwah52525@gmail.com>
2021-11-27 14:00:00 +0100

    solved leetcode problem

---
+++ Day-127/index.js
@@ -0,0 +1,22 @@
+var moveZeroes = function (nums) {
+  for (let i = nums.length - 1; i >= 0; i--) {
+    if (nums[i] === 0) {
+      nums.splice(i, 1);
+      nums.push(0);
+    }
+  }
+};
+
+var twoSum = function (nums, target) {
+  const map = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+    const result = target - num;
+    if (result in map) {
+      return [map[result], i];
+    }
+    map[num] = i;
+  }
+  return [];
+};
