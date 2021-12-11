2665b0820ea2fd5fcb73965ef4b5f119e8cd5224
LalitKushwah <lalitkushwah52525@gmail.com>
2021-12-11 19:27:47 +0530

    valid subsequence

---
+++ Day-141/index.js
@@ -0,0 +1,22 @@
+/**
+ * Valid subsquence
+ * [1,2,3,4,5]
+ * [2,4,5]
+ */
+
+function isValidSubsequence(array, sequence) {
+  let arrayPointer = 0;
+  let sequencePointer = 0;
+  while (arrayPointer < array.length && sequencePointer < sequence.length) {
+    if (array[arrayPointer] !== sequence[sequencePointer]) {
+      arrayPointer++;
+    } else {
+      sequencePointer++;
+      arrayPointer++;
+    }
+  }
+  return sequencePointer === sequence.length;
+}
+
+// Do not edit the line below.
+exports.isValidSubsequence = isValidSubsequence;
