a69965a05deeb216244c1d208ed8bcc49dc346f9
LalitKushwah <lalitkushwah52525@gmail.com>
2021-12-15 20:22:45 +0530

    solved generate document problem

--- Day-145/index.js
+++ Day-145/index.js
@@ -17,3 +17,33 @@ function productSum(array, multiplier = 1) {
   }
   return sum * multiplier;
 }
+
+/**
+ *
+ * Generate Document
+ * if characters frequency >= document frequency
+ *
+ */
+
+function generateDocument(characters, document) {
+  const charMap = {};
+  const docMap = {};
+  for (let i = 0; i < characters.length; i++) {
+    if (charMap[characters[i]])
+      charMap[characters[i]] = charMap[characters[i]] + 1;
+    else charMap[characters[i]] = 1;
+  }
+  for (let i = 0; i < document.length; i++) {
+    if (docMap[document[i]]) docMap[document[i]] = docMap[document[i]] + 1;
+    else docMap[document[i]] = 1;
+  }
+  console.log(docMap);
+  console.log(charMap);
+  const docMapKeys = Object.keys(docMap);
+  for (let i = 0; i < docMapKeys.length; i++) {
+    if (!charMap[docMapKeys[i]]) return false;
+    if (charMap[docMapKeys[i]] < docMap[docMapKeys[i]]) return false;
+  }
+  return true;
+}
+generateDocument("aheaolabbhb", "hello");
